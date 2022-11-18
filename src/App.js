75735172050ff945d72c01270f0553613a951c44
mzabolotnia <m.zabolotnia@brainence.com>
2022-11-18 17:43:00 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {StyleSheet, css} from 'aphrodite-jss'
+
+function App() {
+  return (
+    <div>
+      <span className={css(styles.title)}>double enum</span>
+    </div>
+  );
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+    title: {
+        backgroundColor: 'pink'
+    }
+})
